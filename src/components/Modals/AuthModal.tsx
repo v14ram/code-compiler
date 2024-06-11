2dd9c45f56b0a4ff0e4edf0243816396a3fcd2f4
unknown <svkram46@gmail.com>
2024-06-11 19:18:29 +0530

    Added Login modal

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "@/components/navbar";
 import { IoClose } from "react-icons/io5";
+import Login from "./Login";
 
 type AuthModalProps = {};
 
@@ -19,6 +20,7 @@ const AuthModal:React.F<AuthModalProps> = () => {
 							<IoClose/>
                             </button>
 						</div>
+                        <Login/>
 					</div>
 				</div>
 			</div>
