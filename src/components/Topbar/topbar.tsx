ddf1b65d18dcd169a36709e24bb1d0b5b583de0f
unknown <svkram46@gmail.com>
2024-06-16 06:06:48 +0530

    Timer in navbar

--- src/components/Topbar/topbar.tsx
+++ src/components/Topbar/topbar.tsx
@@ -5,20 +5,48 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import Logout from "../Buttons/Logout";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "@/atoms/authModalAtom";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { BsList } from "react-icons/bs";
+import Timer from "../Timer/timer";
 type TopbarProps = {
-
+	problemsPage?: boolean;
 };
 
-const TopbarProps: React.FC<TopbarProps> = () =>{
-    const [user] = useAuthState(auth);
+const TopbarProps: React.FC<TopbarProps> = ({problemsPage}) => {
+	const [user] = useAuthState(auth);
 	const setAuthModalState = useSetRecoilState(authModalState);
-    return(
-<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
-			<div className={`flex w-full items-center justify-between max-w-[1200px] mx-auto`}>
+
+	return (
+		<nav className='relative flex h-[50px] w-full shrink-0 items-center px-5 bg-dark-layer-1 text-dark-gray-7'>
+			<div className={`flex w-full items-center justify-between ${!problemsPage ? "max-w-[1200px] mx-auto" : ""}`}>
 				<Link href='/' className='h-[22px] flex-1'>
 					<img src='/logo-full.png' alt='Logo' className='h-full' />
 				</Link>
+				{problemsPage && (
+					<div className='flex items-center gap-4 flex-1 justify-center'>
+						<div
+							className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer'
 
+						>
+							<FaChevronLeft />
+						</div>
+						<Link
+							href='/'
+							className='flex items-center gap-2 font-medium max-w-[170px] text-dark-gray-8 cursor-pointer'
+						>
+							<div>
+								<BsList />
+							</div>
+							<p>Problem List</p>
+						</Link>
+						<div
+							className='flex items-center justify-center rounded bg-dark-fill-3 hover:bg-dark-fill-2 h-8 w-8 cursor-pointer'
+
+						>
+							<FaChevronRight />
+						</div>
+					</div>
+				)}
 				<div className='flex items-center space-x-4 flex-1 justify-end'>
 					<div>
 						<a
@@ -27,19 +55,20 @@ const TopbarProps: React.FC<TopbarProps> = () =>{
 							rel='noreferrer'
 							className='bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange hover:bg-dark-fill-2'
 						>
-						 <img src='/coffee.png' alt='Buy me a coffee' className='w-6 h-6 inline-block mr-2' />
-						 Buy me a coffee 
+							<img src='/coffee.png' alt='Buy me a coffee' className='w-6 h-6 inline-block mr-2' />
+							Buy me a coffee
 						</a>
 					</div>
-                    {!user && (
+					{!user && (
 						<Link
-							href='/auth' onClick={ () => {
+							href='/auth' onClick={() => {
 								setAuthModalState((prev) => ({ ...prev, isOpen: true, type: "login" }));
 							}}>
 							<button className='bg-dark-fill-3 py-1 px-2 cursor-pointer rounded '>Sign In</button>
 						</Link>
 					)}
-                    {user && (
+					{problemsPage && <Timer/>}
+					{user && (
 						<div className='cursor-pointer group relative'>
 							<img src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full' />
 							<div
@@ -49,14 +78,14 @@ const TopbarProps: React.FC<TopbarProps> = () =>{
 							>
 								<p className='text-sm'>{user.email}</p>
 							</div>
-                            </div>
-                                )}
-								{user && <Logout/>}
+						</div>
+					)}
+					{user && <Logout />}
 				</div>
 			</div>
-</nav>
+		</nav>
 
-	
-    );
+
+	);
 };
 export default TopbarProps;
