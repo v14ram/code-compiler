61528e8edd558504af511bb72e51466ca774066e
unknown <svkram46@gmail.com>
2024-06-17 18:03:01 +0530

    Title,Constraints,TC - with error

---
+++ src/components/Workspace/Playground/Playground.tsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import PreferenceNav from './PreferenceNav/PreferenceNav';
+import Split from 'react-split';
+import { vscodeDark } from "@uiw/codemirror-theme-vscode";
+import { javascript } from "@codemirror/lang-javascript";
+import CodeMirror from "@uiw/react-codemirror";
+import EditorFooter from './EditorFooter';
+
+
+type PlaygroundProps = {
+
+};
+
+const Playground: React.FC<PlaygroundProps> = () => {
+
+    return (
+        <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
+            <PreferenceNav />
+
+            <Split className='h-[calc(100vh-94px)]' direction='vertical' sizes={[60, 40]} minSize={60}>
+                <div className='w-full overflow-auto'>
+                    <CodeMirror
+                        // value={userCode}
+                        value="const a=1;"
+                        theme={vscodeDark}
+                        // onChange={onChange}
+                        extensions={[javascript()]}
+                        style={{ fontSize: 16 }}
+                    />
+                </div>
+                <div className='w-full px-5 overflow-auto'>
+                    {/* testcase heading */}
+                    <div className='flex h-10 items-center space-x-6'>
+                        <div className='relative flex h-full flex-col justify-center cursor-pointer'>
+                            <div className='text-sm font-medium leading-5 text-white'>Testcases</div>
+                            <hr className='absolute bottom-0 h-0.5 w-full rounded-full border-none bg-white' />
+                        </div>
+                    </div>
+
+                    <div className='flex'>  
+                        {/* {problem.examples.map((example, index) => ( */}
+                        <div
+                            className='mr-2 items-start mt-2 text-white'
+                        // key={example.id}
+                        // onClick={() => setActiveTestCaseId(index)}
+                        >
+                            <div className='flex flex-wrap items-center gap-y-4'>
+                                <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                                    Case 1
+                                </div>
+
+                                {/* <div
+										className={										${activeTestCaseId === index ? "text-white" : "text-gray-500"}
+									`}
+									>
+										Case {index + 1}
+									</div> */}
+                            </div>
+                        </div>
+                        <div
+                            className='mr-2 items-start mt-2 text-white'
+                        // key={example.id}
+                        // onClick={() => setActiveTestCaseId(index)}
+                        >
+                            <div className='flex flex-wrap items-center gap-y-4'>
+                                <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                                    Case 2
+                                </div>
+
+                                {/* <div
+										className={										${activeTestCaseId === index ? "text-white" : "text-gray-500"}
+									`}
+									>
+										Case {index + 1}
+									</div> */}
+                            </div>
+                        </div>
+                        <div
+                            className='mr-2 items-start mt-2 text-white'
+                        // key={example.id}
+                        // onClick={() => setActiveTestCaseId(index)}
+                        >
+                            <div className='flex flex-wrap items-center gap-y-4'>
+                                <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                                    Case 3
+                                </div>
+
+                                {/* <div
+										className={										${activeTestCaseId === index ? "text-white" : "text-gray-500"}
+									`}
+									>
+										Case {index + 1}
+									</div> */}
+                            </div>
+                        </div>
+                        {/* ))} */}
+                    </div>
+
+{/* Input field */}
+
+                    <div className='font-semibold my-4'>
+                        <p className='text-sm font-medium mt-4 text-white'>Input:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                            {/* {problem.examples[activeTestCaseId].inputText} */}
+                        </div>
+                        <p className='text-sm font-medium mt-4 text-white'>Output:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                            {/* {problem.examples[activeTestCaseId].outputText} */}
+                        </div>
+                    </div>
+                </div>
+            </Split>
+            <EditorFooter/>
+        </div>
+    )
+};
+export default Playground;
--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,3 +1,4 @@
+import Workspace from "@/components/Workspace/Workspace";
 import Topbar from '@/components/Topbar/topbar';
 import React from 'react';
 
@@ -9,6 +10,7 @@ const ProblemPage:React.FC<ProblemPageProps> = () => {
     
     return <div>
         <Topbar problemsPage/>
+        <Workspace/>
     </div>
 }
 export default ProblemPage;
