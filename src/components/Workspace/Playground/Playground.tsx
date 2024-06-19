84934ac8daf1362daa7390a52a60dd82e382179c
unknown <svkram46@gmail.com>
2024-06-19 16:33:03 +0530

    Updated t.c UI

--- src/components/Workspace/Playground/Playground.tsx
+++ src/components/Workspace/Playground/Playground.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PreferenceNav from './PreferenceNav/PreferenceNav';
 import Split from 'react-split';
 import { vscodeDark } from "@uiw/codemirror-theme-vscode";
@@ -13,7 +13,7 @@ type PlaygroundProps = {
 };
 
 const Playground: React.FC<PlaygroundProps> = ({ problem }) => {
-
+const [activeTestCaseId,setActiveTestCaseId] = useState<number>(0);
     return (
         <div className='flex flex-col bg-dark-layer-1 relative overflow-x-hidden'>
             <PreferenceNav />
@@ -30,6 +30,7 @@ const Playground: React.FC<PlaygroundProps> = ({ problem }) => {
                     />
                 </div>
                 <div className='w-full px-5 overflow-auto'>
+
                     {/* testcase heading */}
                     <div className='flex h-10 items-center space-x-6'>
                         <div className='relative flex h-full flex-col justify-center cursor-pointer'>
@@ -40,31 +41,36 @@ const Playground: React.FC<PlaygroundProps> = ({ problem }) => {
 
                     <div className='flex'>
                         {problem.examples.map((example, index) => (
-                            <div className='mr-2 items-start mt-2 text-white' key={example.id}>
+                            <div className='mr-2 items-start mt-2 text-white' key={example.id}
+                                onClick={() => setActiveTestCaseId(index)}
+                                >
                                 <div className='flex flex-wrap items-center gap-y-4'>
-                                    <div className='font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap'>
+                                    <div className={`font-medium items-center transition-all focus:outline-none inline-flex bg-dark-fill-3 hover:bg-dark-fill-2 relative rounded-lg px-4 py-1 cursor-pointer whitespace-nowrap
+                                        ${activeTestCaseId ===index ? "text-white" : "text-gray-500"}    
+                                    `}>
                                         Case {index + 1}
                                     </div>
                                 </div>
                             </div>
                         ))}
                     </div>
-                        {/* Input field */}
 
-                        <div className='font-semibold my-4'>
-                            <p className='text-sm font-medium mt-4 text-white'>Input:</p>
-                            <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
-                                {/* {problem.examples[activeTestCaseId].inputText} */}
-                            </div>
-                            <p className='text-sm font-medium mt-4 text-white'>Output:</p>
-                            <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
-                                {/* {problem.examples[activeTestCaseId].outputText} */}
-                            </div>
+
+                    {/* Input field */}
+                    <div className='font-semibold my-4'>
+                        <p className='text-sm font-medium mt-4 text-white'>Input:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                            {problem.examples[activeTestCaseId].inputText}
+                        </div>
+                        <p className='text-sm font-medium mt-4 text-white'>Output:</p>
+                        <div className='w-full cursor-text rounded-lg border px-3 py-[10px] bg-dark-fill-3 border-transparent text-white mt-2'>
+                            {problem.examples[activeTestCaseId].outputText}
                         </div>
                     </div>
+                </div>
             </Split>
             <EditorFooter />
         </div>
-    )
+    );
 };
 export default Playground;
