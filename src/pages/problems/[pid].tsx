70a5dcac6f1c4873e7f80ccfdc0a60abd89b3aed
unknown <svkram46@gmail.com>
2024-06-19 10:27:40 +0530

    Modified at 3:29:58

--- src/pages/problems/[pid].tsx
+++ src/pages/problems/[pid].tsx
@@ -1,16 +1,45 @@
 import Workspace from "@/components/Workspace/Workspace";
 import Topbar from '@/components/Topbar/topbar';
 import React from 'react';
+import { problems } from "@/components/mockProblems/Problems";
 
 type ProblemPageProps = {
-    
+
 };
 
-const ProblemPage:React.FC<ProblemPageProps> = () => {
-    
+const ProblemPage: React.FC<ProblemPageProps> = () => {
+
     return <div>
-        <Topbar problemsPage/>
-        <Workspace/>
+        <Topbar problemsPage />
+        <Workspace />
     </div>
 }
 export default ProblemPage;
+
+export async function getStaticPaths() {
+    const paths = Object.keys(problems).map((key)=> ({
+        params: { pid: key }
+    }))
+
+    return {
+        paths: paths,
+        fallback: false,
+    }
+}
+
+export async function getStaticProps({ params }:{params: {pid:number}}) {
+    const {pid} = params;
+    const problem = problems[pid];
+
+    if(!problem) {
+        return {
+            notFound: true,
+        }
+    }
+
+    return {
+        props:{
+            problem
+        }
+    }
+}
---
+++ src/utils/problems/index.ts
@@ -0,0 +1,24 @@
+import { jumpGame } from "./jump-game";
+import { reverseLinkedList } from "./reverse-linked-list";
+import { search2DMatrix } from "./search-a-2d-matrix";
+import { twoSum } from "./two-sum";
+import { validParentheses } from "./valid-parentheses";
+import {Problem} from "../types/problem";
+
+// interface Problem {
+//     id: string;
+//     description: string;
+//     // Add other properties as needed
+// }
+
+interface ProblemMap {
+    [key: string]: Problem;
+}
+
+export const problems: ProblemMap = {
+    "two-sum": twoSum,
+    "reverse-linked-list": reverseLinkedList,
+    "jump-game": jumpGame,
+    "search-a-2d-matrix": search2DMatrix,
+    "valid-parentheses": validParentheses,
+}
