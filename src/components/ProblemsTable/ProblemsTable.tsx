3e5455fbbb4c941e3f61cc3903a0c833a2770ec3
unknown <svkram46@gmail.com>
2024-06-21 12:18:22 +0530

    Showing data that is to be received in console

--- src/components/ProblemsTable/ProblemsTable.tsx
+++ src/components/ProblemsTable/ProblemsTable.tsx
@@ -5,50 +5,53 @@ import Link from "next/link";
 import { AiFillYoutube } from "react-icons/ai";
 import { IoClose } from "react-icons/io5";
 import YouTube from "react-youtube";
+import { firestore } from "@/firebase/firebase";
+import { collection, getDocs, orderBy, query } from "firebase/firestore";
 
 type ProblemsTableProps = {
-
+    setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const ProblemsTable: React.FC<ProblemsTableProps> = () => {
+const ProblemsTable: React.FC<ProblemsTableProps> = ({ setLoadingProblems }) => {
     const [youtubePlayer, setYoutubePlayer] = useState({
-		isOpen: false,
-		videoId: "",
-	});
+        isOpen: false,
+        videoId: "",
+    });
+    const problems = useGetProblems(setLoadingProblems);
     const closeModal = () => {
-		setYoutubePlayer({ isOpen: false, videoId: "" });
-	};
+        setYoutubePlayer({ isOpen: false, videoId: "" });
+    };
 
-	useEffect(() => {
-		const handleEsc = (e: KeyboardEvent) => {
-			if (e.key === "Escape") closeModal();
-		};
-		window.addEventListener("keydown", handleEsc);
+    useEffect(() => {
+        const handleEsc = (e: KeyboardEvent) => {
+            if (e.key === "Escape") closeModal();
+        };
+        window.addEventListener("keydown", handleEsc);
 
-		return () => window.removeEventListener("keydown", handleEsc);
-	}, []);
+        return () => window.removeEventListener("keydown", handleEsc);
+    }, []);
     return (
         <>
-        <tbody className='text-white'>
-            {problems.map((doc, ind) => {
-                const difficulyColor = doc.difficulty === "Easy" ? "text-dark-green-s" : doc.difficulty === "Medium" ? "text-dark-yellow" : "text-dark-pink";
-                // function setYoutubePlayer(arg0: { isOpen: boolean; videoId: string; }): void {
-                //     throw new Error("Function not implemented.");
-                // }
+            <tbody className='text-white'>
+                {problems.map((doc, ind) => {
+                    const difficulyColor = doc.difficulty === "Easy" ? "text-dark-green-s" : doc.difficulty === "Medium" ? "text-dark-yellow" : "text-dark-pink";
+                    // function setYoutubePlayer(arg0: { isOpen: boolean; videoId: string; }): void {
+                    //     throw new Error("Function not implemented.");
+                    // }
 
-                return (
-                    <tr className={`${ind % 2 == 1 ? 'bg-dark-layer-1' : ''}`} key={doc.id} >
-                        <th className='px-2 py-4 font-medium whitespace-nowrap text-dark-green-s'>
-                            <BsCheckCircle fontSize={"18"} width="18" />
-                        </th>
+                    return (
+                        <tr className={`${ind % 2 == 1 ? 'bg-dark-layer-1' : ''}`} key={doc.id} >
+                            <th className='px-2 py-4 font-medium whitespace-nowrap text-dark-green-s'>
+                                <BsCheckCircle fontSize={"18"} width="18" />
+                            </th>
                             {/* title */}
                             <td className="px-6 py-4">
                                 <Link className="hover:text-blue-600 cursor-pointer" href={`/problems/${doc.id}`}>
-                                {doc.title}
+                                    {doc.title}
                                 </Link>
                             </td>
                             {/* difficulty */}
-							<td className={`px-6 py-4 ${difficulyColor}`}>
+                            <td className={`px-6 py-4 ${difficulyColor}`}>
                                 {doc.difficulty}
                             </td>
                             {/* category */}
@@ -56,40 +59,56 @@ const ProblemsTable: React.FC<ProblemsTableProps> = () => {
                                 {doc.category}
                             </td>
                             {/* video */}
-							<td className={"px-6 py-4"}>
-                            {doc.videoId ? (
-									<AiFillYoutube
-										fontSize={"28"}
-										className='cursor-pointer hover:text-red-600'
-										onClick={() =>
-											setYoutubePlayer({ isOpen: true, videoId: doc.videoId as string })
-										}
-									/>
-								) : (
-									<p className='text-gray-400'>Coming soon</p>
-								)}
-							</td>
-                    </tr>
-                )
-            })
-            }
-        </tbody >
-        {youtubePlayer.isOpen && (
+                            <td className={"px-6 py-4"}>
+                                {doc.videoId ? (
+                                    <AiFillYoutube
+                                        fontSize={"28"}
+                                        className='cursor-pointer hover:text-red-600'
+                                        onClick={() =>
+                                            setYoutubePlayer({ isOpen: true, videoId: doc.videoId as string })
+                                        }
+                                    />
+                                ) : (
+                                    <p className='text-gray-400'>Coming soon</p>
+                                )}
+                            </td>
+                        </tr>
+                    )
+                })
+                }
+            </tbody >
+            {youtubePlayer.isOpen && (
 
-        <tfoot className='fixed top-0 left-0 h-screen w-screen flex items-center justify-center ' >
-		<div className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute' onClick={closeModal}></div>
-		<div className='w-full z-50 h-full px-6 relative max-w-4xl'>
-			<div className='w-full h-full flex items-center justify-center relative'>
-				<div className='w-full relative'>
-					<IoClose fontSize={"35"} className='cursor-pointer absolute -top-16 right-0' onClick={closeModal}/>
-					<YouTube videoId={youtubePlayer.videoId} loading='lazy' iframeClassName='w-full min-h-[500px]' />
-				</div>
-			</div>
-		</div>
-        </tfoot>
-        )}
+                <tfoot className='fixed top-0 left-0 h-screen w-screen flex items-center justify-center ' >
+                    <div className='bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute' onClick={closeModal}></div>
+                    <div className='w-full z-50 h-full px-6 relative max-w-4xl'>
+                        <div className='w-full h-full flex items-center justify-center relative'>
+                            <div className='w-full relative'>
+                                <IoClose fontSize={"35"} className='cursor-pointer absolute -top-16 right-0' onClick={closeModal} />
+                                <YouTube videoId={youtubePlayer.videoId} loading='lazy' iframeClassName='w-full min-h-[500px]' />
+                            </div>
+                        </div>
+                    </div>
+                </tfoot>
+            )}
         </>
-    )
+    );
 }
 
 export default ProblemsTable;
+
+function useGetProblems(setLoadingProblems: React.Dispatch<React.SetStateAction<boolean>>) {
+    const [problems,setProblems]=useState([]);
+
+    useEffect(()=>{
+        const getProblems=async()=>{
+            //fetching data logic
+            setLoadingProblems(true);
+            const q = query(collection(firestore,"problems"),orderBy("order","asc"))
+            const querySnapshot=await getDocs(q);
+            console.log(querySnapshot);
+        }
+        getProblems()
+    },[]);
+    return problems;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,11 @@
 import ProblemsTable from "@/components/ProblemsTable/ProblemsTable";
 import Topbar from "@/components/Topbar/topbar";
-import { firestore } from "@/firebase/firebase";
-import { doc, setDoc } from "firebase/firestore";
 import { useState } from "react";
 
 
+
 export default function Home() {
-  const [inputs, setInputs] = useState({
-    id: '',
-    title: '',
-    difficulty: '',
-    category: '',
-    videoId: '',
-    link: '',
-    order: 0,
-    likes: 0,
-    dislikes: 0,
-  })
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputs({
-      ...inputs,
-      [e.target.name]: e.target.value
-    })
-  }
-  const handleSubmit = async (e: React.FormEvent<HTMLInputElement>) => {
-    e.preventDefault();
-    // convert inouts.order to integer
-    const newProblem={
-      ...inputs,
-      order:Number(inputs.order),
-    }
-    await setDoc(doc(firestore, "problems", inputs.id),inputs);
-    alert("Saved to database");
-  }
+  const [loadingProblems, setLoadingProblems] = useState(true);
   return (
     <>
       <main
@@ -45,11 +18,18 @@ export default function Home() {
           &ldquo; CONSISTENCY IS THE KEY TO SUCCESS &rdquo; 👇
         </h1>
         <div className='relative overflow-x-auto mx-auto px-6 pb-10'>
+          {loadingProblems && (
+            <div className='max-w-[1200px] mx-auto sm:w-7/12 w-full animate-pulse'>
+              {[...Array(10)].map((_, idx) => (
+                <LoadingSkeleton key={idx} />
+              ))}
+            </div>
+          )}
           <div className='max-w-[1200px] mx-auto sm:w-7/12 w-full animate-pulse'>
 
           </div>
           <table className='text-sm text-left text-gray-500 dark:text-gray-400 sm:w-7/12 w-full max-w-[1200px] mx-auto'>
-            <thead className='text-xs text-gray-700 uppercase dark:text-gray-400 border-b '>
+            {!loadingProblems && (<thead className='text-xs text-gray-700 uppercase dark:text-gray-400 border-b '>
               <tr>
                 <th scope='col' className='px-1 py-3 w-0 font-medium'>
                   Status
@@ -68,22 +48,12 @@ export default function Home() {
                   Solution
                 </th>
               </tr>
-            </thead>
-            <ProblemsTable />
+            </thead>)}
+            <ProblemsTable setLoadingProblems={setLoadingProblems} />
           </table>
         </div>
-        <form className='p-6 flex flex-col max-w-sm gap-3' onSubmit={handleSubmit}>
-          <input onChange={handleInputChange} type='text' placeholder='problem id' name='id' />
-          <input onChange={handleInputChange} type='text' placeholder='title' name='title' />
-          <input onChange={handleInputChange} type='text' placeholder='difficulty' name='difficulty' />
-          <input onChange={handleInputChange} type='text' placeholder='category' name='category' />
-          <input onChange={handleInputChange} type='text' placeholder='order' name='order' />
-          <input onChange={handleInputChange} type='text' placeholder='videoId?' name='videoId' />
-          <input onChange={handleInputChange} type='text' placeholder='link?' name='link' />
 
-          <button className="bg-white">Save to db</button>
-        </form>
-      </main>
+      </main >
     </>
   );
 }
@@ -91,3 +61,14 @@ export default function Home() {
 
 // import { initializeApp } from "firebase/app";
 // import { getFirestore } from "firebase/firestore";
+const LoadingSkeleton = () => {
+  return (
+    <div className='flex items-center space-x-12 mt-4 px-6'>
+      <div className='w-6 h-6 shrink-0 rounded-full bg-dark-layer-1'></div>
+      <div className='h-4 sm:w-52  w-32  rounded-full bg-dark-layer-1'></div>
+      <div className='h-4 sm:w-52  w-32 rounded-full bg-dark-layer-1'></div>
+      <div className='h-4 sm:w-52 w-32 rounded-full bg-dark-layer-1'></div>
+      <span className='sr-only'>Loading...</span>
+    </div>
+  );
+};
